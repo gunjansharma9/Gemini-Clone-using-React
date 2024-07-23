8d7b076658dbce2089df1ae7650adf60d654d05c
gunjansharma9 <gunjansharmahapur@gmail.com>
2024-07-24 03:10:42 +0530

    Update with new changes

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import "./Sidebar.css"
 import {assets} from "../../assets/assets"
+import { Context } from '../../context/Context';
 
 const Sidebar = () => {
     const [extended,setExtended] = useState(false);
+    const {onSent,prevPrompts,setRecentPrompt} = useContext(Context)
     return (
         <div className='sidebar'>
             <div className='top'>
@@ -15,11 +17,16 @@ const Sidebar = () => {
 
                 {extended ? 
                 <div className="recent">
-                    <div className="recent-title">Recent</div>
-                    <div className="recent-entry">
-                        <img src={assets.message_icon} alt="" />
-                        <p>What is react...</p>
-                    </div>
+                    <p className="recent-title">Recent</p>
+                    {prevPrompts.map((item,index) => {
+                        return(
+                            <div className="recent-entry">
+                                <img src={assets.message_icon} alt="" />
+                                <p>{item.slice(0,18)} ...</p>
+                            </div>
+                        )
+                    })}
+                    
                 </div>
                 :null
                 }
